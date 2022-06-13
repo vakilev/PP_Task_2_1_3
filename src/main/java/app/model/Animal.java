98fa0e3368e4f0348f501105e3e8177e394cbb5b
Arthur <44236735+vakilev@users.noreply.github.com>
2022-06-13 23:45:28 +0500

    ready steady GOOOO

--- src/main/java/app/model/Animal.java
+++ src/main/java/app/model/Animal.java
@@ -1,4 +1,9 @@
 package app.model;
 
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public abstract class Animal {
+
 }
--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -1,17 +1,26 @@
 package app.model;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AnimalsCage {
+    private Animal cat;
+    private Animal dog;
+    private Timer timer;
 
     @Autowired
-    private Animal animal;
+    public AnimalsCage(@Qualifier("cat") Animal cat, @Qualifier("dog") Animal dog, Timer timer) {
+        this.cat = cat;
+        this.dog = dog;
+        this.timer = timer;
+    }
 
     public void whatAnimalSay() {
         System.out.println("Say:");
-        System.out.println(animal.toString());
+        System.out.println(cat.toString());
+        System.out.println(dog.toString());
         System.out.println("At:");
         System.out.println(new Timer().getTime());
         System.out.println("________________________");
